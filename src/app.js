4f41f7ec07add78f5f88311fde6c5ef06ba8f587
LucasAluC <lucasfedealustiza@gmail.com>
2023-11-07 15:16:44 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -8,6 +9,5 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.listen(8001, () => 
-console.log("Levantando un servidor con Express"));
+app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}`));
 
